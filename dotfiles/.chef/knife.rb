ea4e8e535015ea238c923415cf2caa6892e9cb40
Tom Adams <tom@dxw.com>
2018-05-30 10:11:29 -0400

    Add chef config

---
+++ dotfiles/.chef/knife.rb
@@ -0,0 +1,6 @@
+current_dir = File.dirname(__FILE__)
+log_level                :info
+log_location             STDOUT
+node_name                "tomdxw"
+client_key               "/workbench/home/.chef/key.pem"
+chef_server_url          "https://chef.dxw.net:4001"
