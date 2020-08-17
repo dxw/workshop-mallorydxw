94b91ad2144736b6d28b2ba1476a9087c19baa25
Mallory Adams <mallory@dxw.com>
2020-08-17 10:55:24 -0400

    Add dalmatian-tools

--- install-extra-bits.sh
+++ install-extra-bits.sh
@@ -23,3 +23,7 @@ git -C ${SRC} clone --quiet git@git.govpress.com:dxw/log-tail && \
   composer --working-dir=${SRC}/log-tail install && \
   ln -s ../log-tail-settings-generator/settings.php ${SRC}/log-tail/ && \
   ln -s ${SRC}/log-tail/bin/log-tail ${BIN}/
+
+# dalmatian-tools
+git -C ${SRC} clone --quiet git@github.com:dxw/dalmatian-tools.git && \
+  ln -s ${SRC}/dalmatian-tools/bin/* ${BIN}/
